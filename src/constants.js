37db8016a413c7a2d2a2db11c4646a6a54f08c47
VosKoen <Koen82@gmail.com>
2019-02-27 10:47:56 +0100

    changed baseUrl

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 
-export const baseUrl = "http://localhost:4000";
+export const baseUrl = "https://tum-server.herokuapp.com";
 export const localStorageJwtKey = "currentUserJwt";
 
 export const imagePlaceholder =
