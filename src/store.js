7b32110a90ad47efa818a8b953a83b53a52c20e4
VosKoen <Koen82@gmail.com>
2019-02-27 12:29:53 +0100

    disabled devtools

--- src/store.js
+++ src/store.js
@@ -3,12 +3,13 @@ import reducer from "./reducers";
 import { storeJwt } from "./middleware";
 import ReduxThunk from "redux-thunk";
 
-const devTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const devTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const enhancer = compose(
-  applyMiddleware(ReduxThunk, storeJwt),
-  devTools
+  applyMiddleware(ReduxThunk, storeJwt)
+  // ,
+  // devTools
 );
 
 const store = createStore(reducer, enhancer);
