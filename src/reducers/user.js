e245e106e7aa8eb85f6a2161e745eb3f37e2d41e
VosKoen <Koen82@gmail.com>
2019-03-11 12:11:21 +0100

    added account manage page and password reset

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,26 +1,40 @@
-import {USER_LOGIN_SUCCESS, USER_LOGOUT} from '../actions/users'
-import {localStorageJwtKey} from '../constants'
+import {
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  SET_USER_PROFILE_DATA,
+  SET_NEW_PASSWORD_FAILED,
+  SET_NEW_PASSWORD_SUCCESS
+} from "../actions/users";
+import { localStorageJwtKey } from "../constants";
 
-let initialState = null
+let initialState = null;
 try {
-  const jwt = localStorage.getItem(localStorageJwtKey)
+  const jwt = localStorage.getItem(localStorageJwtKey);
   if (jwt) {
-    initialState = { jwt }
+    initialState = { jwt };
   }
-}
-catch (e) {
-  console.log(`Error retrieving data from local storage`, e)
+} catch (e) {
+  console.log(`Error retrieving data from local storage`, e);
 }
 
-export default function (state = initialState, {type, payload}) {
-	switch (type) {
-		case USER_LOGIN_SUCCESS:
-			return payload
+export default function(state = initialState, { type, payload }) {
+  switch (type) {
+    case USER_LOGIN_SUCCESS:
+      return payload;
 
     case USER_LOGOUT:
-      return null
+      return null;
 
-		default:
-      return state
-	}
+    case SET_USER_PROFILE_DATA:
+      return { ...state, ...payload, resetPasswordFailMessage: null , resetPasswordSuccessMessage: null};
+
+    case SET_NEW_PASSWORD_FAILED:
+      return { ...state, resetPasswordFailMessage: payload, resetPasswordSuccessMessage: null };
+
+    case SET_NEW_PASSWORD_SUCCESS:
+      return { ...state, resetPasswordFailMessage: null, resetPasswordSuccessMessage: "Password changed" };
+
+    default:
+      return state;
+  }
 }
